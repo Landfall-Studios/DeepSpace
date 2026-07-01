b14fa49a144c6b84cfa6a01bc139c5bee06f625c
Mallow Horton <mallowhorton@mallowhorton.com>
2026-07-01 00:48:20 -0500

    Nerf the Angel Block in Space

--- src/main/java/world/landfall/deepspace/item/AngelBlockItem.java
+++ src/main/java/world/landfall/deepspace/item/AngelBlockItem.java
@@ -1,25 +1,30 @@
 package world.landfall.deepspace.item;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
+import net.minecraft.world.inventory.tooltip.TooltipComponent;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import world.landfall.deepspace.Deepspace;
 import world.landfall.deepspace.ModBlocks;
 import world.landfall.deepspace.ModItems;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
 
 public class AngelBlockItem extends BlockItem {
     public AngelBlockItem(Properties properties) {
@@ -38,6 +43,14 @@ public class AngelBlockItem extends BlockItem {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
         var result = super.use(level, player, usedHand);
+
+        if (level.dimension().location().equals(Deepspace.path("space")) && !player.isCreative()) {
+            player.displayClientMessage(
+                    Component.literal("The gravity here isn't strong enough...").withStyle(ChatFormatting.RED), true
+            );
+            return InteractionResultHolder.fail(player.getItemInHand(usedHand));
+        }
+
         if (result.getResult() == InteractionResult.PASS) {
             var direction = player.getLookAngle();
             var reach = player.blockInteractionRange();
@@ -57,6 +70,14 @@ public class AngelBlockItem extends BlockItem {
                 (int)Math.round(input.x),
                 (int)Math.round(input.y),
                 (int)Math.round(input.z)
+        );
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        tooltipComponents.add(
+                Component.literal("Can be placed in mid air if there is sufficient gravity").withStyle(ChatFormatting.ITALIC, ChatFormatting.DARK_GRAY)
         );
     }
 }
